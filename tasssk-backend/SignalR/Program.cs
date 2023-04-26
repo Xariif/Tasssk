7b96cf4d626a1d1f2a8f51a17f98eb47263cd7a9
Bocian <59657029+Xariif@users.noreply.github.com>
2023-04-26 16:24:23 +0200

    Docker configs

--- tasssk-backend/SignalR/Program.cs
+++ tasssk-backend/SignalR/Program.cs
@@ -1,25 +1,21 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using ServerSignalR;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddSignalR();
+builder.Services.AddCors();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+app.UseCors(builder =>
+builder
+ .AllowAnyMethod()
+ .AllowAnyHeader()
+ .AllowAnyOrigin()
+);
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
+app.MapHub<NotificationsHub>("notifications");
 
 app.Run();
